e4f342afc9dbec372f52eced82e0fa997bee797b
Ubuntu <ubuntu@ip-172-31-19-224.ec2.internal>
2024-12-10 17:21:08 +0000

    new

--- src/components/giftTicket/giftTicket.tsx
+++ src/components/giftTicket/giftTicket.tsx
@@ -17,28 +17,30 @@ import {
 } from "@/components/ui/dialog"
 import { Button } from "../ui/button"
 
-// import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
 
-export interface SearchResults{
+export interface SearchResults {
     name: string | undefined,
     email: string | undefined,
     picture: string | undefined | null
 }
 
-export default function GiftTicket(){
+export default function GiftTicket() {
     const [results, setResults] = useState<SearchResults[]>([])
     const [searchTerm, setSearchTerm] = useState("");
 
-    async function search(value: string){
+    async function search(value: string) {
         setSearchTerm(value);
         if (value.trim() === "") {
-        setResults([]); // Clear results if search input is empty
-        return;
+            setResults([]); // Clear results if search input is empty
+            return;
         }
         const search = await SearchUser(value);
         setResults(search);
     }
-    return(
+    return (
         <div className="flex flex-col space-y-3">
             {/* title and description */}
             <div className="">
@@ -46,7 +48,7 @@ export default function GiftTicket(){
                     Gift Ticket
                 </h2>
                 <p className="text-muted-foreground">
-                    You can easily give out free tickets to people by 
+                    You can easily give out free tickets to people by
                     simply searching their name or email address
                 </p>
             </div>
@@ -66,29 +68,29 @@ export default function GiftTicket(){
                     <div>
                         <h3 className="font-semibold mt-3">Search Results</h3>
                         <ul className="space-y-2 w-full bg-accent/60 p-5 rounded-2xl">
-                        {results.map((result, index) => (
-                            <li key={index} className="w-full flex items-center justify-between">
-                                <div className="flex items-center space-x-3">
-                                    {result.picture && (
-                                        <div className="w-10 h-10 rounded-full relative">
-                                            <Image
-                                                src={result.picture}
-                                                alt={result.name || "User"}
-                                                className="object-cover rounded-full"
-                                                fill
-                                            />
+                            {results.map((result, index) => (
+                                <li key={index} className="w-full flex items-center justify-between">
+                                    <div className="flex items-center space-x-3">
+                                        {result.picture && (
+                                            <div className="w-10 h-10 rounded-full relative">
+                                                <Image
+                                                    src={result.picture}
+                                                    alt={result.name || "User"}
+                                                    className="object-cover rounded-full"
+                                                    fill
+                                                />
+                                            </div>
+                                        )}
+                                        <div>
+                                            <p className="font-medium">{result.name || "No Name"}</p>
+                                            <p className="text-sm text-muted-foreground">
+                                                {result.email}
+                                            </p>
                                         </div>
-                                    )}
-                                    <div>
-                                        <p className="font-medium">{result.name || "No Name"}</p>
-                                        <p className="text-sm text-muted-foreground">
-                                        {result.email}
-                                        </p>
                                     </div>
-                                </div>
-                                <GiftUser userName={result.name} />
-                            </li>
-                        ))}
+                                    <GiftUser userName={result.name} />
+                                </li>
+                            ))}
                         </ul>
                     </div>
                 )}
@@ -108,27 +110,30 @@ export default function GiftTicket(){
 
 // modal for registered user
 
-function GiftUser(props:{
+function GiftUser(props: {
     userName: string | undefined
-}){
+}) {
     return (
         <Dialog>
-        <DialogTrigger asChild>
-            <EllipsisVertical className="stroke-accent-foreground cursor-pointer" />
-        </DialogTrigger>
-        <DialogContent className="sm:max-w-[425px] flex flex-col justify-start">
-          <DialogHeader>
-            <DialogTitle className="flex space-x-3">
-                {`Gift a ticket to ${props.userName}`}</DialogTitle>
-            <DialogDescription>
-              Make changes to your profile here. Click save when you're done.
-            </DialogDescription>
-          </DialogHeader>
-          
-          <DialogFooter className="sm:justify-start">
-            <Button type="submit">Save changes</Button>
-          </DialogFooter>
-        </DialogContent>
-      </Dialog>
+            <DialogTrigger asChild>
+                <EllipsisVertical className="stroke-accent-foreground cursor-pointer" />
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-[425px] flex flex-col justify-start">
+                <DialogHeader>
+                    <DialogTitle className="flex space-x-3">
+                        {`Gift a ticket to ${props.userName}`}</DialogTitle>
+                    <DialogDescription>
+                        Make changes to your profile here. Click save when you're done.
+                    </DialogDescription>
+                </DialogHeader>
+
+                <DialogFooter className="sm:justify-start">
+                    <form>
+
+                    </form>
+                    <Button type="submit">Save changes</Button>
+                </DialogFooter>
+            </DialogContent>
+        </Dialog>
     )
 }
