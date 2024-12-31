9b128dc804b108c3803eda1c96c602022e860fbc
ijijohn356@gmail.com <ijijohn356@gmail.com>
2024-12-31 00:02:41 +0000

    merging

---
+++ src/services/giftTicket/createAccount.ts
@@ -0,0 +1,27 @@
+'use server'
+import prisma from '@/lib/prisma'
+
+// function to create the user
+export async function createNewUser(name: string, email: string) {
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        email
+      }
+    })
+    if (!user) {
+      const newUser = await prisma.user.create({
+        data: {
+          name,
+          email
+        }
+      })
+
+      return newUser
+    }
+  } catch (error) {
+    throw new Error(`${error}`)
+  } finally {
+    await prisma.$disconnect()
+  }
+}
