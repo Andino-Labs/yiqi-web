a003e2aca90ce602d4d82a375c3b7dec2a4116e7
ijijohn356@gmail.com <ijijohn356@gmail.com>
2024-12-31 00:11:15 +0000

    merging

--- src/services/actions/event/getEvent.ts
+++ src/services/actions/event/getEvent.ts
@@ -1,20 +1,29 @@
 'use server'
 
-import { SavedEventType } from '@/schemas/eventSchema'
 import prisma from '@/lib/prisma'
-import {
-  SavedEventSchema,
-  GetEventFilterSchemaType
-} from '@/schemas/eventSchema'
+import { SavedEventSchema } from '@/schemas/eventSchema'
 
 export async function getEvent({
   eventId,
   includeTickets
-}: GetEventFilterSchemaType): Promise<SavedEventType> {
+}: {
+  eventId: string
+  includeTickets?: boolean
+}) {
   const event = await prisma.event.findUniqueOrThrow({
     where: { id: eventId },
-    include: includeTickets ? { tickets: true } : undefined
+    include: { tickets: includeTickets || false }
   })
 
-  return SavedEventSchema.parse(event)
+  const formattedEvent = {
+    ...event,
+    tickets: event.tickets?.map(ticket => ({
+      ...ticket,
+      price: ticket.price.toNumber() // Convert Decimal to number
+    }))
+  }
+
+  return SavedEventSchema.parse(formattedEvent)
 }
+
+export type EventType = Awaited<ReturnType<typeof getEvent>>
