38155fab4bf658d519e3d40cab42a41979aafb3c
cnerylozada <cnerylozada@gmail.com>
2025-01-29 18:54:02 -0500

    refact: organization-layout works as nextjs layout

--- src/app/(protected)/admin/organizations/[id]/settings/page.tsx
+++ src/app/(protected)/admin/organizations/[id]/settings/page.tsx
@@ -1,7 +1,6 @@
 import { getUser } from '@/lib/auth/lucia'
 import { redirect } from 'next/navigation'
 import { Roles } from '@prisma/client'
-import OrganizationLayout from '@/components/orgs/OrganizationLayout'
 import { getOrganization } from '@/services/actions/organizationActions'
 import OrganizationSettings from '@/components/settings/organizationSettings/organizationSettings'
 
@@ -13,24 +12,11 @@ export default async function Settings({ params }: { params: { id: string } }) {
   }
   if (user.role === Roles.ADMIN) {
     return (
-      <main className="flex flex-col items-center justify-center dark:bg-[rgb(28, 28, 28)]">
-        <OrganizationLayout
-          orgId={params.id}
-          userProps={{
-            picture: user.picture!,
-            email: user.email,
-            name: user.name,
-            id: user.id
-          }}
-        >
-          {organization && (
-            <OrganizationSettings
-              userId={user.id}
-              organization={organization}
-            />
-          )}
-        </OrganizationLayout>
-      </main>
+      <>
+        {organization && (
+          <OrganizationSettings userId={user.id} organization={organization} />
+        )}
+      </>
     )
   } else if (user.role === Roles.NEW_USER) {
     redirect('/newuser')
