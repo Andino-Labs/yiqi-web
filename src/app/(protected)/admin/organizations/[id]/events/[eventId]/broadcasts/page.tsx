775cc9ec3d955b39f4927b48269526ab436a4191
cnerylozada <cnerylozada@gmail.com>
2025-01-30 10:27:20 -0500

    feat: attendes, broadcasts, registration and summary pages were created

---
+++ src/app/(protected)/admin/organizations/[id]/events/[eventId]/broadcasts/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div>
+      <div>Broadcast page</div>
+    </div>
+  )
+}
---
+++ src/app/(protected)/admin/organizations/[id]/events/[eventId]/summary/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div>
+      <div>Summary page</div>
+    </div>
+  )
+}
