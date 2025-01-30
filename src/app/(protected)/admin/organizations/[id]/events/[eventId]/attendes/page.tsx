8e7f556cd2d4c14fe8d0cb8b61c05e10003c3686
cnerylozada <cnerylozada@gmail.com>
2025-01-30 10:57:39 -0500

    feat: render information in attendes page

--- src/app/(protected)/admin/organizations/[id]/events/[eventId]/attendes/page.tsx
+++ src/app/(protected)/admin/organizations/[id]/events/[eventId]/attendes/page.tsx
@@ -1,7 +1,19 @@
-export default function Page() {
+import EventRegistrationTable from '@/components/events/EventRegistrationTable'
+import { getEventRegistrations } from '@/services/actions/event/getEventAttendees'
+import { getTranslations } from 'next-intl/server'
+
+export default async function Page({
+  params
+}: {
+  params: { eventId: string }
+}) {
+  const t = await getTranslations('DeleteAccount')
+  const attendees = await getEventRegistrations(params.eventId)
+
   return (
     <div>
-      <div>Attendes page</div>
+      <h2 className="text-xl font-semibold mb-2">{t('eventRegistrations')}</h2>
+      <EventRegistrationTable registrations={attendees} />
     </div>
   )
 }
