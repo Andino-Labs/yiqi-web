b466a7b4b49cce7f41855a6d30b984938f19e8c3
Daniel151505 <daniel150520011@gmail.com>
2025-03-02 17:25:30 -0500

    fix: Added getTwitterVariables

---
+++ src/services/actions/management-tool/channels/twitter/getTwitterVariables.ts
@@ -0,0 +1,9 @@
+'use server'
+
+export const getTwitterVariables = async () => {
+  return Boolean(
+    process.env.X_API_KEY &&
+      process.env.X_API_SECRET &&
+      process.env.X_BEARER_TOKEN
+  )
+}
