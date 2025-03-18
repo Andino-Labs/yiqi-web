b15ef7a46eef2d17168dcc698f9bff0be0b54687
Daniel151505 <daniel150520011@gmail.com>
2025-03-17 20:00:56 -0500

    fix linter

--- src/app/api/x/callback/route.ts
+++ src/app/api/x/callback/route.ts
@@ -1,66 +1,80 @@
-import { NextResponse } from 'next/server';
-import { cookies } from 'next/headers';
+import { NextResponse } from 'next/server'
+import { cookies } from 'next/headers'
 
-const X_CLIENT_ID = process.env.X_CLIENT_ID as string;
-const X_CLIENT_SECRET = process.env.X_CLIENT_SECRET as string;
+const X_CLIENT_ID = process.env.X_CLIENT_ID as string
+const X_CLIENT_SECRET = process.env.X_CLIENT_SECRET as string
 const REDIRECT_URI = `${process.env.NEXT_PUBLIC_URL}${process.env.NEXT_PUBLIC_X_REDIRECT_URI}`
 
 export async function GET(request: Request) {
-  const { searchParams } = new URL(request.url);
-  const code = searchParams.get('code');
+  const { searchParams } = new URL(request.url)
+  const code = searchParams.get('code')
 
   if (!code) {
-    return NextResponse.json({ error: 'authorization code not received' }, { status: 400 });
+    return NextResponse.json(
+      { error: 'authorization code not received' },
+      { status: 400 }
+    )
   }
 
-  const codeVerifier = cookies().get('code_twitter_verifier')?.value;
+  const codeVerifier = cookies().get('code_twitter_verifier')?.value
   if (!codeVerifier) {
-    return NextResponse.json({ error: 'code_verifier not found' }, { status: 400 });
+    return NextResponse.json(
+      { error: 'code_verifier not found' },
+      { status: 400 }
+    )
   }
 
-  const credentials = Buffer.from(`${X_CLIENT_ID}:${X_CLIENT_SECRET}`).toString('base64');
+  const credentials = Buffer.from(`${X_CLIENT_ID}:${X_CLIENT_SECRET}`).toString(
+    'base64'
+  )
 
   const tokenResponse = await fetch('https://api.twitter.com/2/oauth2/token', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded',
-      'Authorization': `Basic ${credentials}`,
+      Authorization: `Basic ${credentials}`
     },
     body: new URLSearchParams({
       grant_type: 'authorization_code',
       code,
       redirect_uri: REDIRECT_URI,
-      code_verifier: codeVerifier,
-    }).toString(),
-  });
-  
-  const tokenData = await tokenResponse.json();
+      code_verifier: codeVerifier
+    }).toString()
+  })
+
+  const tokenData = await tokenResponse.json()
 
   if (!tokenResponse.ok) {
-    return NextResponse.json({ error: 'No se pudo obtener el token' }, { status: 401 });
+    return NextResponse.json(
+      { error: 'No se pudo obtener el token' },
+      { status: 401 }
+    )
   }
 
-  const { access_token, refresh_token } = tokenData;
+  const { access_token, refresh_token } = tokenData
 
   const userResponse = await fetch('https://api.twitter.com/2/users/me', {
     headers: {
-      Authorization: `Bearer ${access_token}`,
-    },
+      Authorization: `Bearer ${access_token}`
+    }
   })
 
-  const userData = await userResponse.json();
+  const userData = await userResponse.json()
 
-  const { id, username } = userData.data;
+  const { id, username } = userData.data
 
   if (!userResponse.ok) {
     return NextResponse.json({ error: 'User not found' }, { status: 401 })
   }
 
-  const redirectUrl = new URL('/admin/organizations/channels/twitter', request.url);
-  redirectUrl.searchParams.append('accessToken', access_token);
-  redirectUrl.searchParams.append('refreshToken', refresh_token);
+  const redirectUrl = new URL(
+    '/admin/organizations/channels/twitter',
+    request.url
+  )
+  redirectUrl.searchParams.append('accessToken', access_token)
+  redirectUrl.searchParams.append('refreshToken', refresh_token)
   redirectUrl.searchParams.append('userId', id)
   redirectUrl.searchParams.append('screenName', username)
 
-  return NextResponse.redirect(redirectUrl);
+  return NextResponse.redirect(redirectUrl)
 }
