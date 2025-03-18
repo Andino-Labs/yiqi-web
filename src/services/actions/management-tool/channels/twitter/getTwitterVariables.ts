aada7331094b5a45d495991b2c054afa9ce06297
Daniel151505 <daniel150520011@gmail.com>
2025-03-17 22:28:29 -0500

    feat: Update verify twitter variables

--- src/services/actions/management-tool/channels/twitter/getTwitterVariables.ts
+++ src/services/actions/management-tool/channels/twitter/getTwitterVariables.ts
@@ -1,9 +1,5 @@
 'use server'
 
 export const verifyTwitterVariables = async () => {
-  return Boolean(
-    process.env.X_API_KEY &&
-      process.env.X_API_SECRET &&
-      process.env.X_BEARER_TOKEN
-  )
+  return Boolean(process.env.X_CLIENT_ID && process.env.X_CLIENT_SECRET)
 }
